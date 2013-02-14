25edf527860e02e7cea1e46e22018cc386fe12c5
Phillip Calvin <phillip.calvin@eleostech.com>
2013-02-14 12:37:04 -0500

    Instantiate singleton using self.class so it's easier to make subclasses

--- Library/JCNotificationBannerPresenterIOSStyle.m
+++ Library/JCNotificationBannerPresenterIOSStyle.m
@@ -11,7 +11,7 @@
   static JCNotificationBannerPresenter* sharedPresenter = nil;
   static dispatch_once_t onceToken;
   dispatch_once(&onceToken, ^{
-    sharedPresenter = [JCNotificationBannerPresenterIOSStyle new];
+    sharedPresenter = [self.class new];
   });
   return sharedPresenter;
 }
